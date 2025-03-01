277881dd49c35b5742b3b52a8298e58e78f91da5
Mohd Mahmodi <141009456+MohdYahyaMahmodi@users.noreply.github.com>
2025-02-28 17:28:32 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const process = require('process');
 const parser = require('ua-parser-js');
 const { uniqueNamesGenerator, animals, colors } = require('unique-names-generator');
 const WebSocket = require('ws');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
 
 // Handle SIGINT
 process.on('SIGINT', () => {
@@ -15,15 +19,194 @@ process.on('SIGTERM', () => {
   process.exit(0);
 });
 
+// MIME types for serving static files
+const mimeTypes = {
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.css': 'text/css',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpg',
+  '.svg': 'image/svg+xml',
+  '.mp3': 'audio/mpeg',
+  '.ogg': 'audio/ogg',
+  '.ico': 'image/x-icon'
+};
+
+// Create HTTP server
+const server = http.createServer((req, res) => {
+  // Parse URL
+  const parsedUrl = url.parse(req.url);
+  
+  // Extract the path
+  let pathname = path.join(__dirname, 'public', parsedUrl.pathname);
+  
+  // Maps "/" to "/index.html"
+  if (pathname === path.join(__dirname, 'public', '/')) {
+    pathname = path.join(__dirname, 'public', '/index.html');
+  }
+
+  // Read the file
+  fs.readFile(pathname, (err, data) => {
+    if (err) {
+      // If the file is not found
+      res.statusCode = 404;
+      res.end(`File ${pathname} not found!`);
+      return;
+    }
+
+    // Get file extension
+    const ext = path.parse(pathname).ext;
+    
+    // Set Content-type and send the data
+    res.setHeader('Content-type', mimeTypes[ext] || 'text/plain');
+    res.end(data);
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3002;
+server.listen(PORT, () => {
+  console.log(`HTTP Server running on port ${PORT}`);
+});
+
+class Peer {
+    constructor(socket, request) {
+        // set socket
+        this.socket = socket;
+
+        // set remote ip
+        this._setIP(request);
+
+        // set peer id
+        this._setPeerId(request);
+        // is WebRTC supported?
+        this.rtcSupported = request.url.indexOf('webrtc') > -1;
+        // set name 
+        this._setName(request);
+        // for keepalive
+        this.timerId = 0;
+        this.lastBeat = Date.now();
+    }
+
+    _setIP(request) {
+        if (request.headers['x-forwarded-for']) {
+            this.ip = request.headers['x-forwarded-for'].split(/\s*,\s*/)[0];
+        } else {
+            this.ip = request.connection.remoteAddress;
+        }
+        // IPv4 and IPv6 use different values to refer to localhost
+        if (this.ip == '::1' || this.ip == '::ffff:127.0.0.1') {
+            this.ip = '127.0.0.1';
+        }
+    }
+
+    _setPeerId(request) {
+        if (request.peerId) {
+            this.id = request.peerId;
+        } else {
+            this.id = request.headers.cookie && request.headers.cookie.includes('peerid=') 
+                ? request.headers.cookie.split('peerid=')[1].split(';')[0]
+                : Peer.uuid();
+        }
+    }
+
+    toString() {
+        return `<Peer id=${this.id} ip=${this.ip} rtcSupported=${this.rtcSupported}>`;
+    }
+
+    _setName(req) {
+        let ua = parser(req.headers['user-agent']);
+
+        let deviceName = '';
+        
+        if (ua.os && ua.os.name) {
+            deviceName = ua.os.name.replace('Mac OS', 'Mac') + ' ';
+        }
+        
+        if (ua.device && ua.device.model) {
+            deviceName += ua.device.model;
+        } else if (ua.browser && ua.browser.name) {
+            deviceName += ua.browser.name;
+        }
+
+        if(!deviceName)
+            deviceName = 'Unknown Device';
+
+        const displayName = uniqueNamesGenerator({
+            length: 2,
+            separator: ' ',
+            dictionaries: [colors, animals],
+            style: 'capital',
+            seed: this.id.hashCode()
+        });
+
+        this.name = {
+            model: ua.device ? ua.device.model : null,
+            os: ua.os ? ua.os.name : null,
+            browser: ua.browser ? ua.browser.name : null,
+            type: ua.device ? ua.device.type : null,
+            deviceName,
+            displayName
+        };
+    }
+
+    getInfo() {
+        return {
+            id: this.id,
+            name: this.name,
+            rtcSupported: this.rtcSupported
+        }
+    }
+
+    // return uuid of form xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx
+    static uuid() {
+        let uuid = '',
+            ii;
+        for (ii = 0; ii < 32; ii += 1) {
+            switch (ii) {
+                case 8:
+                case 20:
+                    uuid += '-';
+                    uuid += (Math.random() * 16 | 0).toString(16);
+                    break;
+                case 12:
+                    uuid += '-';
+                    uuid += '4';
+                    break;
+                case 16:
+                    uuid += '-';
+                    uuid += (Math.random() * 4 | 8).toString(16);
+                    break;
+                default:
+                    uuid += (Math.random() * 16 | 0).toString(16);
+            }
+        }
+        return uuid;
+    }
+}
+
+Object.defineProperty(String.prototype, 'hashCode', {
+  value: function() {
+    var hash = 0, i, chr;
+    for (i = 0; i < this.length; i++) {
+      chr = this.charCodeAt(i);
+      hash = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  }
+});
+
 class DrplServer {
-    constructor(port) {
-        this._wss = new WebSocket.Server({ port: port });
+    constructor(server) {
+        this._wss = new WebSocket.Server({ server, path: '/server/webrtc' });
         this._wss.on('connection', (socket, request) => this._onConnection(new Peer(socket, request)));
         this._wss.on('headers', (headers, response) => this._onHeaders(headers, response));
 
         this._rooms = {};
 
-        console.log('Drpl.co is running on port', port);
+        console.log('Drpl.co WebSocket server is ready');
     }
 
     _onConnection(peer) {
@@ -129,9 +312,9 @@ class DrplServer {
 
     _send(peer, message) {
         if (!peer) return;
-        if (this._wss.readyState !== this._wss.OPEN) return;
+        if (peer.socket.readyState !== WebSocket.OPEN) return;
         message = JSON.stringify(message);
-        peer.socket.send(message, error => '');
+        peer.socket.send(message, error => {});
     }
 
     _keepAlive(peer) {
@@ -157,134 +340,5 @@ class DrplServer {
     }
 }
 
-class Peer {
-    constructor(socket, request) {
-        // set socket
-        this.socket = socket;
-
-        // set remote ip
-        this._setIP(request);
-
-        // set peer id
-        this._setPeerId(request);
-        
-        // is WebRTC supported?
-        this.rtcSupported = request.url.indexOf('webrtc') > -1;
-        
-        // set name 
-        this._setName(request);
-        
-        // for keepalive
-        this.timerId = 0;
-        this.lastBeat = Date.now();
-    }
-
-    _setIP(request) {
-        if (request.headers['x-forwarded-for']) {
-            this.ip = request.headers['x-forwarded-for'].split(/\s*,\s*/)[0];
-        } else {
-            this.ip = request.connection.remoteAddress;
-        }
-        // IPv4 and IPv6 use different values to refer to localhost
-        if (this.ip == '::1' || this.ip == '::ffff:127.0.0.1') {
-            this.ip = '127.0.0.1';
-        }
-    }
-
-    _setPeerId(request) {
-        if (request.peerId) {
-            this.id = request.peerId;
-        } else {
-            this.id = request.headers.cookie.replace('peerid=', '');
-        }
-    }
-
-    toString() {
-        return `<Peer id=${this.id} ip=${this.ip} rtcSupported=${this.rtcSupported}>`;
-    }
-
-    _setName(req) {
-        let ua = parser(req.headers['user-agent']);
-
-        let deviceName = '';
-        
-        if (ua.os && ua.os.name) {
-            deviceName = ua.os.name.replace('Mac OS', 'Mac') + ' ';
-        }
-        
-        if (ua.device.model) {
-            deviceName += ua.device.model;
-        } else {
-            deviceName += ua.browser.name;
-        }
-
-        if(!deviceName)
-            deviceName = 'Unknown Device';
-
-        const displayName = uniqueNamesGenerator({
-            length: 2,
-            separator: ' ',
-            dictionaries: [colors, animals],
-            style: 'capital',
-            seed: this.id.hashCode()
-        });
-
-        this.name = {
-            model: ua.device.model,
-            os: ua.os.name,
-            browser: ua.browser.name,
-            type: ua.device.type,
-            deviceName,
-            displayName
-        };
-    }
-
-    getInfo() {
-        return {
-            id: this.id,
-            name: this.name,
-            rtcSupported: this.rtcSupported
-        };
-    }
-
-    // return uuid of form xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx
-    static uuid() {
-        let uuid = '', ii;
-        for (ii = 0; ii < 32; ii += 1) {
-            switch (ii) {
-                case 8:
-                case 20:
-                    uuid += '-';
-                    uuid += (Math.random() * 16 | 0).toString(16);
-                    break;
-                case 12:
-                    uuid += '-';
-                    uuid += '4';
-                    break;
-                case 16:
-                    uuid += '-';
-                    uuid += (Math.random() * 4 | 8).toString(16);
-                    break;
-                default:
-                    uuid += (Math.random() * 16 | 0).toString(16);
-            }
-        }
-        return uuid;
-    }
-}
-
-// Add hashCode method to String prototype for generating consistent names
-Object.defineProperty(String.prototype, 'hashCode', {
-    value: function() {
-        var hash = 0, i, chr;
-        for (i = 0; i < this.length; i++) {
-            chr = this.charCodeAt(i);
-            hash = ((hash << 5) - hash) + chr;
-            hash |= 0; // Convert to 32bit integer
-        }
-        return hash;
-    }
-});
-
-// Start the server
-const server = new DrplServer(process.env.PORT || 3002);
+// Create WebSocket server using the same HTTP server
+const drplServer = new DrplServer(server);
